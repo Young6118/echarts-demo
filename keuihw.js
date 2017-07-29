0c53ee0b84b88ca674b9fd795582bac28b3aec36
yilishang1997 <zy1308536472@163.com>
2017-07-29 22:50:22 +0800

    init

---
+++ keuihw.js
@@ -0,0 +1,11 @@
+// 引入 echarts 主模块
+const echarts = require('echarts/lib/echarts')
+// 引入柱状图
+require('echarts/lib/chart/bar')
+// 引入提示框
+require('echarts/lib/component/tooltip')
+
+require('echarts/lib/component/title')
+
+// 基于准备好的 dom 初始化 echarts 实例
+// 
